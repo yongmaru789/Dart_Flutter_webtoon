ad7d27b173093c81621ac09f440df9257c0e4953
JAEUNGCHOI <jaeung0129@gmail.com>
2026-02-06 19:17:45 +0900

    에피소드 기능 추가 및 웹툰 브라우저 연결

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:today_webtoon/models/webtoon_detail_model.dart';
 import 'package:today_webtoon/services/api_service.dart';
 import 'package:today_webtoon/models/webtoon_episode_model.dart';
+import 'package:today_webtoon/widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
 
@@ -45,77 +46,94 @@ class _DetailScreenState extends State<DetailScreen> {
           ),          
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox (
-            height: 50,
-          ),
-          Row (
-            mainAxisAlignment: MainAxisAlignment.center,            
-            children: [
-              Hero (
-                tag: widget.id,
-                child: Container (
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black
-                      )
-                    ]
+      body: SingleChildScrollView (
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            children: [              
+              Row (
+                mainAxisAlignment: MainAxisAlignment.center,            
+                children: [
+                  Hero (
+                    tag: widget.id,
+                    child: Container (
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black
+                          )
+                        ]
+                      ),
+                      child: Image.network(
+                        widget.thumb,
+                        headers: const {
+                          "User-Agent":
+                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        },
+                      ),
+                    ),
                   ),
-                  child: Image.network(
-                    widget.thumb,
-                    headers: const {
-                      "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                    },
-                  ),
-                ),
+                ],    
               ),
-            ],    
-          ),
-          const SizedBox(
-            height: 25
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if(snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric (
-                    horizontal: 50
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snapshot.data!.about,
-                        style: TextStyle(
-                          fontSize: 16,                      
+              const SizedBox(
+                height: 25
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if(snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: TextStyle(
+                            fontSize: 16,                      
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                        style: TextStyle(
-                          fontSize: 16,                      
+                        const SizedBox(
+                          height: 15,
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-              return Text("...");
-            }
-          )
-        ],
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: TextStyle(
+                            fontSize: 16,                      
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return Text("...");
+                }
+              ),
+              const SizedBox (
+                height: 25,
+              ),
+              FutureBuilder (
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        for(var episode in snapshot.data!) 
+                        Episode(
+                          episode: episode, 
+                          webtoonId: widget.id, 
+                        ),
+                      ],
+                    );
+                  }    
+                  return Container();         
+                } ,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
